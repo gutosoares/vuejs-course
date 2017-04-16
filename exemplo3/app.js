ed2ccf19fe91a401010b52c06f10142114ab01b1
Augusto Soares <gutosoares@outlook.com>
2017-04-16 20:04:37 -0300

    Exemplo 3

---
+++ exemplo3/app.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+    data: {
+        title: 'Hello World VueJS!! :)'
+    },
+    methods: {
+        sayHello: function() {
+            return 'Hello!!';
+        }
+    }
+});
